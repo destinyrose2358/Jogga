776e3bc3283861f2c0594a542b081333ebe71e9d
ray-leun1 <ray.leun1@gmail.com>
2019-11-11 10:36:10 -0800

    Restructure Nav.js

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dev:cgLCDvdnDZa4JscM@cluster0-3vxfv.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'eb1ff734-0410-11ea-8d71-362b9e155667'
+};
