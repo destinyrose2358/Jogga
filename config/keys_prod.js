ac0d4c0e455cb46d021e0f6febcfe618bbfb20f4
Alexander <alexcrisel4@gmail.com>
2019-11-12 16:34:51 -0800

    Adds aws for profile picture uploads

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  secretOrKey: process.env.SECRET_OR_KEY,
+  AWSSecretKey: process.env.AWS_SECRET_KEY,
+  AWSAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  AWSRegion: process.env.AWS_REGION
 }
