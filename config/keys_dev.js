fecd30eec699dfc074549320e130427f27657e6e
Alexander <alexcrisel4@gmail.com>
2019-11-15 12:48:44 -0800

    Removes keys_dev

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://dev:cgLCDvdnDZa4JscM@cluster0-3vxfv.mongodb.net/test?retryWrites=true&w=majority',
-  secretOrKey: 'eb1ff734-0410-11ea-8d71-362b9e155667',
-  AWSSecretKey: "gmnTDJPhenr6JfxhBxi5hXDqDbiKaQWc6Q3KaQPN",
-  AWSAccessKeyId: "AKIAJCLGFTJTSYCMYAVA",
-  AWSRegion: "us-east-2"
-};
